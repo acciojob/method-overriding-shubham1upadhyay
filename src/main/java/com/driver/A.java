004df4013f383fc7344194feae0d9e6678108cb7
Shubham Upadhyay <113086164+shubham1upadhyay@users.noreply.github.com>
2023-01-02 11:13:28 +0530

    Update A.java

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -1,8 +1,11 @@
-public class A
+
+//this is a parent class
+
+public class A       
 {
-public String meth(String s)
+public String meth()
 {
-  s = "Invoking method from class A";
+  String s = "Invoking method from class A";
   return s;
 }
 }
