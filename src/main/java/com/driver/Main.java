12097a4d515bfea6fca2d7260f77cd26722dbb73
Shubham Upadhyay <113086164+shubham1upadhyay@users.noreply.github.com>
2023-01-02 11:22:12 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,22 @@ package com.driver;
 
 public class Main {
   
+  public static void main(String[] args)
+  {
+    
+   /* now creating an object of class B to call a method of class A
+    before over-ride the method it calls the method of A*/
+    
+    B b = new B();
+    String methodOfClassA = b.meth();
+    System.out.println(methodOfClassA);
+    
+    
+    /* when we over-ride the method in class B , then after creating an object
+     to call , then it always calls that over-ridden */
+    
+    
+    
+  }
+  
 }
